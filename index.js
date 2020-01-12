9c81a0c56531b515579a080dddbf2b2edfccde09
Felix He <felixhe97@gmail.com>
2020-01-11 18:42:46 -0800

    moved lib one layer up

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 'use strict';
 
-module.exports.graph = require('./src/api');
+module.exports = {
+    "Algo" : {
+        "heapsort": require('./algo/heapsort'),
+        "mergesort": require('./algo/mergesort'),
+        "quickselect": require('./algo/quickselect'),
+        "quicksort": require('./algo/quicksort')
+    },
+    "DS" : {
+        "adjacencylist": require('./ds/adjacencylist'),
+        "binaryheap": require('./ds/binaryheap'),
+        "deque": require('./ds/deque'),
+        "trie": require('./ds/trie'),
+        "unionfind": require('./ds/unionfind')
+    }
+};
