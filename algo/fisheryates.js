094803d5ad5ce2c2ec8f738cedfb1ee257172fd3
Felix He <felixhe97@gmail.com>
2020-03-20 00:03:53 -0700

    fisher yates completed

--- algo/fisheryates.js
+++ algo/fisheryates.js
@@ -4,18 +4,18 @@ const getIndex = require('../utils/getRand').getRandomIntInclusive;
 const swap = require('../utils/swapElements');
 
 /**
- * Shuffles elements in array, fisher-yates implementation
+ * Shuffles array elements in-place, fisher-yates algorithm
  *
- * @param {Array<*>} arr - array of elements to shuffle completely
+ * @param {Array<*>} arr - array of elements to shuffle
  * @param {function} [rng] - function returning floating point in range [0, 1)
  */
-module.exports = function(arr, rng = Math.random){
-    if (arr && Array.isArray(arr)) {
-        for (let i = arr.length - 1; i >= 0; --i){
-            let index = getIndex(0, i, rng);
-            swap(arr, i, index);
+module.exports = function(arr, rng = Math.random) {
+    if (arr && Array.isArray(arr) && rng && typeof rng === "function") {
+        for (let i = arr.length - 1; i > 0; --i){
+            swap(arr, i, getIndex(0, i, rng));
         }
         return true;
+    } else {
+        return false;
     }
-    return false;
 }
